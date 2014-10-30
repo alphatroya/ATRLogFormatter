6f51078bfff3e195a7e6fd0d4bb1c33f2c9a5d57
Alexey Korolev <alphatroya@gmail.com>
2014-10-30 20:52:52 +0300

    add all files

---
+++ ATRLogFormatter/ATRLogFormatter.h
@@ -0,0 +1,11 @@
+//
+// Created by Королев Алексей on 19.08.14.
+// Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CocoaLumberjack/DDLog.h>
+
+
+@interface ATRLogFormatter : NSObject <DDLogFormatter>
+@end
