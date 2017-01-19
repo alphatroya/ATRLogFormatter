144e757193b8a2e65ce12a5ab19be287913742e0
Alexey Korolev <alphatroya@gmail.com>
2017-01-19 19:44:17 +0300

    Fix Carthage installation

--- Example/Pods/Target Support Files/ATRLogFormatter/ATRLogFormatter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ATRLogFormatter : NSObject
-@end
-@implementation PodsDummy_ATRLogFormatter
-@end
--- Example/Pods/Target Support Files/Pods-ATRLogFormatterDemo/Pods-ATRLogFormatterDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ATRLogFormatterDemo : NSObject
-@end
-@implementation PodsDummy_Pods_ATRLogFormatterDemo
-@end
